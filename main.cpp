9d6fe73ecbd77d1685fa8a2635899b4617d1feca
kimhoyul <chhc007@gmail.com>
2021-12-22 13:52:29 +0900

    비주얼 스튜디오 삼각형 널아 구하기

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Height = 0;
+	int Base = 0;
+	float Triangle = 0;
+
+	cout << "높이를 입력해주세요." << endl;
+	cin >> Height;
+	cout << "밑변을 입력해주세요." << endl;
+	cin >> Base;
+
+	Triangle = (
+		static_cast<float>(Height) * static_cast<float>(Base)
+		) / 2.0f;
+
+	cout << "삼각형의 넓이는 " << Triangle << " 입니다." << endl;
+
+	return 0;
+}
